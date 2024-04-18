ea056637a304a4cdaff30054563e3f5a558eeb98
MuhammedHussein3 <156622887+MuhammedHussein3@users.noreply.github.com>
2024-04-18 19:35:14 +0200

    My First commit

---
+++ src/main/java/com/twt/snaptweet/SnapTweetApplication.java
@@ -0,0 +1,13 @@
+package com.twt.snaptweet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapTweetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnapTweetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/twt/snaptweet/SnapTweetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twt.snaptweet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapTweetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
